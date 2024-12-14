e35fcd143fc0c969a5dde507ab185680174d570d
Josh <josh.ja.carter@gmail.com>
2024-12-14 21:32:07 +1300

    Tidying behaviour is a little bit less stupid

--- Assets/Entity/Behaviour/Worker/Tidy__Collect.cs
+++ Assets/Entity/Behaviour/Worker/Tidy__Collect.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -14,36 +15,16 @@ public class Tidy__Collect : State {
     ReadOnlyCollection<ItemEntity> itemEntities;
     ItemEntity targetEntity;
     Path path;
-    int TARGET_ATTEMPTS = 5;
+    int TARGET_ATTEMPTS = 10;
 
     int step, stepsMax;
     static readonly int stepSpeed = 15;
 
 
-
     public override void OnEntry() {
         animator.Play(anim.name);
 
-        itemEntities = EntityManager.Instance.GetItemEntities();
-
-        if (itemEntities.Count == 0) {
-            CompleteState();
-            return;
-        }
-
-        for (int i = 0 ; i < TARGET_ATTEMPTS ; i += 1) {
-            targetEntity = itemEntities[Random.Range(0, itemEntities.Count)];
-
-            path = Pathfind.FindPath(transform.position, targetEntity.transform.position);
-            if (path != null) {
-                step = 0;
-                stepsMax = path.Count * stepSpeed;
-                return;
-            }
-        }
-        
-        // Couldn't find a path to an item - failure to find ANY item
-        CompleteState(false);
+        TryFindPath();
     }
 
     public override void FixedRun() {
@@ -82,5 +63,45 @@ public class Tidy__Collect : State {
         
         // Path is invalidated - but there may exist another valid path so don't return failure here
         else CompleteState();
+    }
+
+    void TryFindPath() {
+
+        itemEntities = EntityManager.Instance.GetItemEntities();
+
+        if (itemEntities.Count == 0) {
+            CompleteState();
+            return;
+        }
+
+        // Choose some (limited) random item entities
+        List<ItemEntity> randomTargets = new List<ItemEntity>(TARGET_ATTEMPTS);
+
+        for (int i = 0 ; i < TARGET_ATTEMPTS && i < itemEntities.Count ; i += 1) {
+            ItemEntity target = itemEntities[Random.Range(0, itemEntities.Count)];
+
+            randomTargets.Add(target);
+        }
+
+        // Order them by proximity to the agent; we shouldn't be going for items 20000 tiles away when there is one next to us
+        Vector2 pos = entity.transform.position;
+        Vector2Int gridPos = new Vector2Int((int) Math.Floor(pos.x), (int) Math.Floor(pos.y));
+        List<ItemEntity> orderedTargets = randomTargets
+                .OrderBy(entity => Math.Pow(entity.transform.position.x - gridPos.x, 2) + Math.Pow(entity.transform.position.y - gridPos.y, 2))
+                .ToList();
+        
+        // Choose the closest one that is reachable
+        foreach (ItemEntity target in orderedTargets) {
+            path = Pathfind.FindPath(transform.position, target.transform.position);
+            if (path != null) {
+                targetEntity = target;
+                step = 0;
+                stepsMax = path.Count * stepSpeed;
+                return;
+            }
+        }
+        
+        // Couldn't find a path to an item - failure to find ANY item
+        CompleteState(false);
     }
 }
